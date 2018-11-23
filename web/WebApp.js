d0361f910934519995805ca5ccc88cedd4bcddd0
Michael James <thelivingpc@gmail.com>
2018-11-22 22:23:38 -0500

    revert

--- web/WebApp.js
+++ web/WebApp.js
@@ -106,8 +106,6 @@ try {
     obj['Timestamp'] = new Date();
   }
 
-  console.log(obj);
-
   if (typeof obj['Status'] !== 'undefined') {
     chgStatus(obj.row, obj['Status']);
   }
@@ -123,7 +121,7 @@ try {
     return typeof obj[header] !== 'undefined' ? obj[header] : data[index]
   })
 
-  if (!newRow[getColNumByName(sh, "row") - 1]) { 
+  if (newRow[getColNumByName(sh, "row") - 1]) { 
     newRow[getColNumByName(sh, "row") - 1] = SpreadsheetApp.openById(ssID).getSheetByName('Queue').getLastRow() + 1;
   }
   
