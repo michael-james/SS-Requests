329aa71ea88f6ee7e5c2789cc54b967c9f2eacc2
Michael James <thelivingpc@gmail.com>
2018-11-23 20:52:42 -0500

    updated secrets

--- core/secrets.js
+++
@@ -1,6 +0,0 @@
-var ssID = "15jyBvxt6NU1QYpy__Syb3HleXizLp5QYWTqJei20Dkc";
-
-var calendarID = "ert.com_b1qei7dur3ai02niq7ptl1mp4c@group.calendar.google.com"; // CST&L Screenshot Requests calendar
-var calendarIDTest = "ert.com_mton57ftcg81g2llrtbrqaifb0@group.calendar.google.com"; // Test calendar GMT+0
-
-var taskListID = "MDc4NDk4NDEzNTA5MTk3MzE3MzM6Mjc1MzYwMTAxNDA3MTQ5Mjow"; //michael.james
